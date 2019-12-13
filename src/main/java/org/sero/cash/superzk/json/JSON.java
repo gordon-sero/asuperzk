1d554363eb1afa54412e0a139e66909642cecf69
bourneAc <bourne@gameternal.com>
2019-12-13 10:58:51 +0800

    update

--- src/main/java/org/sero/cash/superzk/json/JSON.java
+++ src/main/java/org/sero/cash/superzk/json/JSON.java
@@ -5,6 +5,7 @@ import org.sero.cash.superzk.protocol.AccountType;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -16,6 +17,7 @@ public class JSON {
 
     static {
         mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
         SimpleModule module = new SimpleModule();
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
